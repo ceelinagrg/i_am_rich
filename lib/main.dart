7e0c342e147a48fa4d466d21a3796cdf07df8f9a
ceelinagrg <gsalina950@gmail.com>
2019-12-25 11:50:32 +0545

    comment

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+//main function is used to run the app on startup
+void main() => runApp(MaterialApp(
+  theme:ThemeData.dark(),
+  home: Scaffold(
+    backgroundColor: Colors.blueGrey,
+    appBar: AppBar(
+      title: Text("I am rich"),
+      backgroundColor: Colors.blueGrey,
+    ),
+    body:Center(
+  child: Image.asset("images/diamond.png"))),
+));
